fcf5a55be4d31bd7be10dc537ecab1a7fa5011bb
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-20 19:41:48 +0300

    ending while loops

--- 2013-OOProgrammingWithJava-PART1/week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ 2013-OOProgrammingWithJava-PART1/week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -1,11 +1,40 @@
+
 import java.util.Scanner;
 
 public class LoopsEndingRemembering {
+
     public static void main(String[] args) {
         // program in this project exercises 36.1-36.5
         // actually this is just one program that is split in many parts
-        
-        Scanner reader = new Scanner(System.in);
 
+        Scanner reader = new Scanner(System.in);
+        int number = 0;
+        int sum = 0;
+        int count = 0;
+        int even = 0;
+        int odd = 0;
+        while (number != -1) {
+
+            sum += number;
+
+            System.out.println("Type numbers");
+            number = Integer.parseInt(reader.nextLine());
+
+            if (number != -1) {
+                count++;
+                if (number % 2 == 0) {
+                    even++;
+                } else {
+                    odd++;
+                }
+            }
+
+        }
+        System.out.println("Thank you and see you later!");
+        System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + count);
+        System.out.println("Average: " + ((double) sum / count));
+        System.out.println("Even numbers " + even);
+        System.out.println("Odd numbers " + odd);
     }
 }
