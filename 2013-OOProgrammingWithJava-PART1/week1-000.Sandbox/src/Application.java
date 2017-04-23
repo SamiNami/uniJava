826970d33a7f3c68940ecde899e68e9a0476456c
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-23 20:51:57 +0300

    for each loops, arrays, sorting

--- 2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
+++ 2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
@@ -1,14 +1,29 @@
 // This is not an exercise but a "sandbox" where you can freely test
 // whatever you want
+import java.util.ArrayList;
 
 public class Application {
 
     public static void main(String[] args) {
+        ArrayList<String> teachers = new ArrayList<String>();
 
-        // shift alt f = format
-        // sout tab
-        // ctrl + space = autocomplete
-        // ctrl and r to rename things fast
+        teachers.add("Anthony");
+        teachers.add("Paul");
+        teachers.add("John");
+        teachers.add("Martin");
+        teachers.add("Matt");
 
+        for (String kappa : teachers) {
+            System.out.println(kappa);
+        }
     }
 }
+
+//public class Application {
+//  public static void main(String[] args) {
+// shift alt f = format
+// sout tab
+// ctrl + space = autocomplete
+// ctrl and r to rename things fast
+// }
+// }
--- 2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ 2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+        
+        while(true){
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
+            
+            if(word.isEmpty()){
+                break;                
+            }
+            else{
+                words.add(word);
+            }
+            
+        }
+        
+        Collections.reverse(words);
+        
+        for(String word : words){
+            System.out.println(word);
+        }
+        
     }
 }
