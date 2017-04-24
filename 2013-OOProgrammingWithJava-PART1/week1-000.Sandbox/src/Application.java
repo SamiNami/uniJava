63a6ea2037c93e23bf0a0293217bb846e39e121c
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-24 15:31:05 +0300

    objects and classes

--- 2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
+++ 2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
@@ -5,19 +5,23 @@ import java.util.ArrayList;
 public class Application {
 
     public static void main(String[] args) {
-        ArrayList<String> teachers = new ArrayList<String>();
+    ArrayList<String> cities = new ArrayList<String>();
+    ArrayList<String> countries = new ArrayList<String>();
 
-        teachers.add("Anthony");
-        teachers.add("Paul");
-        teachers.add("John");
-        teachers.add("Martin");
-        teachers.add("Matt");
+    countries.add("Finland");
+    countries.add("Germany");
+    countries.add("Netherlands");
 
-        for (String kappa : teachers) {
-            System.out.println(kappa);
-        }
-    }
+    cities.add("Berliini");
+    cities.add("Nijmegen");
+    cities.add("Turku");
+    cities.add("Helsinki");
+
+    System.out.println("number of countries " + countries.size() );
+    System.out.println("number of cities " + cities.size() );
 }
+    }
+
 
 //public class Application {
 //  public static void main(String[] args) {
--- 2013-OOProgrammingWithJava-PART1/week4-072.1.YourFirstAccount/src/Accounts.java
+++ 2013-OOProgrammingWithJava-PART1/week4-072.1.YourFirstAccount/src/Accounts.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account samisAccount = new Account("Sami's acount",100.00);
+        samisAccount.deposit(20);
+        System.out.println(samisAccount);
     }
 
 }
