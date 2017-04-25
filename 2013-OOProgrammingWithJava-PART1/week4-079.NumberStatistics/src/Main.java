13bfd20dccc106c79625a4248829035410e46965
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-25 16:53:39 +0300

    calling a class own methods

--- 2013-OOProgrammingWithJava-PART1/week4-079.NumberStatistics/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week4-079.NumberStatistics/src/Main.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Write test code here
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
-
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
-
-        // The tests does not work if you do not create the objects in the correct order
+        NumberStatistics numStats = new NumberStatistics();
+        NumberStatistics numEven = new NumberStatistics();
+        NumberStatistics numOdd = new NumberStatistics();
+        System.out.println("Type numbers: ");
+        
+        while(true){
+            int number = Integer.parseInt(reader.nextLine());
+            if(number == -1){
+                System.out.println("Sum " + numStats.sum());
+                System.out.println("Even " + numEven.sum());
+                System.out.println("Odd " + numOdd.sum());
+                break;
+            }
+            else{
+                numStats.addNumber(number);
+                if(number % 2 == 0){
+                    numEven.addNumber(number);
+                }
+                else{
+                    numOdd.addNumber(number);
+                }
+            }
+        }
 
     }
 }
