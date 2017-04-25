97c85d4e6efa507758decdb521d50fbc6681b338
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-25 16:22:05 +0300

    made a clock

--- 2013-OOProgrammingWithJava-PART1/week4-078.ClockUsingCounter/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week4-078.ClockUsingCounter/src/Main.java
@@ -2,27 +2,19 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
         BoundedCounter seconds = new BoundedCounter(59);
         BoundedCounter minutes = new BoundedCounter(59);
         BoundedCounter hours = new BoundedCounter(23);
 
-        System.out.print("seconds: ");
-        int s = Integer.parseInt(reader.nextLine());
-        System.out.print("minutes: ");
-        int m = Integer.parseInt(reader.nextLine());
-        System.out.print("hours: ");
-        int h = Integer.parseInt(reader.nextLine());
+        seconds.setValue(50);
+        minutes.setValue(59);
+        hours.setValue(23);
 
-        seconds.setValue(s);
-        minutes.setValue(m);
-        hours.setValue(h);
-
-        int i = 0;
-        while (i < 121) {
-            System.out.println(hours + ":" + minutes + ":" + seconds);
+        while ( true ) {
+            System.out.println( hours + ":" + minutes + ":" + seconds );
+            Thread.sleep(1000);
+            
             seconds.next();
             if (seconds.getValue() == 0) {
                 minutes.next();
@@ -30,9 +22,8 @@ public class Main {
             if(seconds.getValue() == 0 && minutes.getValue() == 0){
                 hours.next();
             }
-
-            i++;
         }
-
     }
 }
+
+
