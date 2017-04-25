fdfd45be7113cd47dc2f84b6a06e8fcf2e8ef13f
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-26 02:25:48 +0300

    another object of the same type as a parameter

--- 2013-OOProgrammingWithJava-PART1/week5-087.ApartmentComparison/src/Apartment.java
+++ 2013-OOProgrammingWithJava-PART1/week5-087.ApartmentComparison/src/Apartment.java
@@ -11,4 +11,32 @@ public class Apartment {
         this.pricePerSquareMeter = pricePerSquareMeter;
     }
     
+    public boolean larger(Apartment otherApartment){
+        return this.squareMeters > otherApartment.squareMeters;
+    }
+    
+    private int price(){
+        return pricePerSquareMeter*squareMeters;
+    }
+    
+    public int priceDifference(Apartment otherApartment){
+        int firstPrice = this.price();
+        int secondPrice = otherApartment.price();
+        int difference = firstPrice - secondPrice;
+        
+        if(difference < 0){
+            difference = -difference;
+        }
+        
+        return  difference;
+    }
+    public boolean moreExpensiveThan(Apartment otherApartment){
+        int firstPrice = this.price();
+        int secondPrice = otherApartment.price();
+        
+        if(firstPrice >= secondPrice){
+            return true;
+        }
+        return false;
+    }
 }
