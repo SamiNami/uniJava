021892ce56afbe075d4aa0e90b53a48147796065
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-26 15:29:25 +0300

    searching objects

--- 2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
@@ -5,6 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write here the main program
+       Scanner reader = new Scanner(System.in);
+       ArrayList<Student> list = new ArrayList<Student>();
+       
+       
+       while(true){
+           System.out.println("name: ");
+           String name = reader.nextLine();
+           
+           if(name.isEmpty()){
+               break;
+           }
+           
+           System.out.println("studentnumber: ");
+           String sNumber = reader.nextLine();
+           
+           Student student = new Student(name,sNumber);
+           list.add(student);
+       }
+       for(Student std : list){
+           System.out.println(std);
+       }
+       
+       System.out.println("Give search term: ");
+       String term = reader.nextLine();
+       System.out.println("Result: ");
+       
+       for(Student std : list){
+           String name = std.getName();
+           if (name.contains(term)){
+               System.out.println(std);
+           }
+       }
+       
     }
 }
