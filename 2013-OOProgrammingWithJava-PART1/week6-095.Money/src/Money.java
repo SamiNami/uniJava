5eb340b97d31a99ca95a2a443e65ca5210419a69
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-27 14:39:54 +0300

    handling money

--- 2013-OOProgrammingWithJava-PART1/week6-095.Money/src/Money.java
+++ 2013-OOProgrammingWithJava-PART1/week6-095.Money/src/Money.java
@@ -33,4 +33,45 @@ public class Money {
         return euros + "." + zero + cents + "e";
     }
 
+    public Money plus(Money added) {
+        int euros = this.euros() + added.euros();
+        int cents = this.cents() + added.cents();
+        
+        if(cents > 99){
+            cents -= 100;
+            euros++;
+        }
+        
+        Money money = new Money(euros, cents);
+        return money;
+    }
+
+    public boolean less(Money compared) {
+        if (this.euros() < compared.euros()) {
+            return true;
+        } else if (this.euros() == compared.euros() && this.cents() < compared.cents()) {
+            return true;
+        }
+        return false;
+    }
+
+    public Money minus(Money decremented) {
+        int euros = this.euros() - decremented.euros();
+        int cents;
+
+        if (this.cents() < decremented.cents()) {
+            euros--;
+            cents = 100 - decremented.cents();
+        } else {
+            cents = this.cents() - decremented.cents();
+        }
+
+        if (euros < 0) {
+            euros = 0;
+            cents = 0;
+        }
+
+        Money moneyleft = new Money(euros, cents);
+        return moneyleft;
+    }
 }
