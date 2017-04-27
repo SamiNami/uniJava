eb83c42fca11425012be53147472aefb7894b520
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-27 16:41:34 +0300

    blocks of code

--- 2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
+++ 2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
@@ -1,27 +1,14 @@
 // This is not an exercise but a "sandbox" where you can freely test
 // whatever you want
-import java.util.ArrayList;
+
+import java.util.*;
 
 public class Application {
 
-    public static void main(String[] args) {
-    ArrayList<String> cities = new ArrayList<String>();
-    ArrayList<String> countries = new ArrayList<String>();
 
-    countries.add("Finland");
-    countries.add("Germany");
-    countries.add("Netherlands");
+    
 
-    cities.add("Berliini");
-    cities.add("Nijmegen");
-    cities.add("Turku");
-    cities.add("Helsinki");
-
-    System.out.println("number of countries " + countries.size() );
-    System.out.println("number of cities " + cities.size() );
 }
-    }
-
 
 //public class Application {
 //  public static void main(String[] args) {
@@ -36,4 +23,3 @@ public class Application {
     variable balance, NetBeans will offer you the opportunity to generate the getter
     and setter methods, and a constructor that sets a starting value for the
     1object variable. */
-
