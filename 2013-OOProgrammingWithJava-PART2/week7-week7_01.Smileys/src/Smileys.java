bb8d5c0d6d147d58fbe3253b26108a552346f9c1
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-30 20:20:08 +0300

    surrounded by smilies, clean code

--- 2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ 2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -2,10 +2,36 @@
 public class Smileys {
 
     public static void main(String[] args) {
-        // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+
+        printWithSmileys("Method");
+        printWithSmileys("Beerbottle");
+        printWithSmileys("Interface");
     }
 
+    private static void printWithSmileys(String characterString) {
+
+        characterString = prepareString(characterString);
+
+        printRow(characterString.length() / 2);
+        System.out.println(characterString);
+        printRow(characterString.length() / 2);
+
+    }
+
+    private static String prepareString(String characterString) {
+        if (characterString.length() % 2 == 1) {
+            characterString += " ";
+        }
+
+        return characterString = ":) " + characterString + " :)";
+    }
+
+    private static void printRow(int length) {
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(":)");
+        }
+
+        System.out.println("");
+    }
 }
