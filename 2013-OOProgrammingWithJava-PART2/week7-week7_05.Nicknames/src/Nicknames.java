04f47938ba076da6a3b2685c42516cfdef91ed76
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-01 16:38:44 +0300

    hashmaps

--- 2013-OOProgrammingWithJava-PART2/week7-week7_05.Nicknames/src/Nicknames.java
+++ 2013-OOProgrammingWithJava-PART2/week7-week7_05.Nicknames/src/Nicknames.java
@@ -5,15 +5,15 @@ import java.util.Map;
 public class Nicknames {
 
     public static void main(String[] args) {
-        
+
         HashMap<String, String> people = new HashMap<String, String>();
-        
+
         people.put("matti", "mage");
         people.put("mikael", "mixu");
         people.put("arto", "arppa");
-        
+
         System.out.println(people.get("mikael"));
-        
+
     }
 
 }
