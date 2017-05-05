636254b8671db7ed6178086d315c544bf0732982
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-05 22:10:38 +0300

    HashSets in HashMaps

---
+++ 2013-OOProgrammingWithJava-PART2/week9-week9_24.MultipleEntryDictionary/src/dictionary/PersonalMultipleEntryDictionary.java
@@ -0,0 +1,40 @@
+
+package dictionary;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+public class PersonalMultipleEntryDictionary implements  MultipleEntryDictionary {
+    
+    private Map<String,Set<String>> dictonary;
+     
+    public PersonalMultipleEntryDictionary (){
+           this.dictonary = new HashMap<String,Set<String>>(); 
+        }
+    
+    @Override
+    public void add(String word, String entry) {
+        if(!this.dictonary.containsKey(word)){
+            dictonary.put(word,new HashSet<String>());
+        }
+        
+        Set<String> wordTranslations = this.dictonary.get(word);
+        wordTranslations.add(entry);
+        
+    }
+
+    @Override
+    public Set<String> translate(String word) {
+         
+        return this.dictonary.get(word);
+    }
+
+    @Override
+    public void remove(String word) {
+        this.dictonary.remove(word);
+    }
+    
+}
