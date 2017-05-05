0c7e4cc057e188ccc804746a9cdcf2101d222b67
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-05 20:16:31 +0300

    wordInspection

--- 2013-OOProgrammingWithJava-PART2/week9-week9_23.WordInspection/src/wordinspection/Main.java
+++ 2013-OOProgrammingWithJava-PART2/week9-week9_23.WordInspection/src/wordinspection/Main.java
@@ -1,14 +1,18 @@
 package wordinspection;
-
+ 
 import java.io.File;
-
+ 
 public class Main {
-
+ 
     public static void main(String[] args) {
         // test your code here
-
+ 
         File file = new File("src/shortList.txt");
         // all words are in file src/wordList.txt
-
+ 
+        WordInspection inspection = new WordInspection(file);
+ 
+        
+        
     }
 }
