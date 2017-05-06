e4447a806b0bab1d1338a24fde49e485821213e2
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-06 18:36:31 +0300

    using sets to prevent duplicates

--- 2013-OOProgrammingWithJava-PART2/week9-week9_25.DuplicateRemover/src/tools/PersonalDuplicateRemover.java
+++ 2013-OOProgrammingWithJava-PART2/week9-week9_25.DuplicateRemover/src/tools/PersonalDuplicateRemover.java
@@ -1,22 +1,16 @@
-
 package tools;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
 
 public class PersonalDuplicateRemover implements DuplicateRemover {
 
     private Set<String> strings = new HashSet<String>();
     private int numberOfDuplicates;
 
-        
-        
     @Override
     public void add(String characterString) {
-          boolean added = strings.add(characterString);
+        boolean added = strings.add(characterString);
         if (!added) {
             numberOfDuplicates++;
         }
@@ -24,18 +18,18 @@ public class PersonalDuplicateRemover implements DuplicateRemover {
 
     @Override
     public int getNumberOfDetectedDuplicates() {
-       return numberOfDuplicates;
+        return numberOfDuplicates;
     }
 
     @Override
     public Set<String> getUniqueCharacterStrings() {
-         return strings;
+        return strings;
     }
 
     @Override
     public void empty() {
         strings.clear();
-        numberOfDuplicates = 0;        
+        numberOfDuplicates = 0;
     }
-    
+
 }
