0d479251f31d0462de5e2d43fcb488f796a3653b
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-08 18:18:39 +0300

    fixed null pointer exception,correct formating for printing etc,and a hashmap for birdobservations

--- 2013-OOProgrammingWithJava-PART2/week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
+++ 2013-OOProgrammingWithJava-PART2/week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
@@ -1,13 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author John
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RingingCentre {
-    
+
+    private Map<Bird, List<String>> observations;
+
+    public RingingCentre() {
+        this.observations = new HashMap<Bird, List<String>>();
+    }
+
+    public void observe(Bird bird, String place) {
+
+        if (!observations.containsKey(bird)) {
+            List<String> places = new ArrayList<String>();
+            places.add(place);
+            this.observations.put(bird, places);
+        } else {
+            this.observations.get(bird).add(place);
+        }
+
+    }
+
+    public void observations(Bird bird) {
+
+        if (observations.containsKey(bird)) {
+            List<String> places = observations.get(bird);
+            System.out.println(bird + " observations: " + places.size());
+
+            for (String place : places) {
+                System.out.println(place);
+            }
+        } else {
+            System.out.println(bird + " observations: 0");
+        }
+    }
 }
