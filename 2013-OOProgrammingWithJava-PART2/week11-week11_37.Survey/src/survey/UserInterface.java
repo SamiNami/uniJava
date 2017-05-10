4173b6433a0a9cc07a9544bfbb330b27de3d08c8
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-10 20:05:37 +0300

    learning more ui stuff

--- 2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
+++ 2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
@@ -10,9 +10,45 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
-        // Create your app here
+        frame = new JFrame("Survey");
+        frame.setPreferredSize(new Dimension(200, 300));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+
     }
 
+    private void createComponents(Container container) {
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+
+        container.add(new JLabel("Are you?"));
+
+        JCheckBox yes = new JCheckBox("Yes!");
+        JCheckBox no = new JCheckBox("No!");
+        container.add(yes);
+        container.add(no);
+
+        container.add(new JLabel("Why?"));
+
+        JRadioButton noRsn = new JRadioButton("No reason.");
+        JRadioButton fun = new JRadioButton("Because it is fun!");
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(noRsn);
+        buttonGroup.add(fun);
+
+        container.add(noRsn);
+        container.add(fun);
+
+        JButton button = new JButton("Done!");
+
+        container.add(button);
+    }
 
     public JFrame getFrame() {
         return frame;
