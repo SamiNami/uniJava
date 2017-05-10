d05bdb67a04ded656db6788607427c8030f8b1f8
Sami Peralahti <sami.peralahti@gmail.com>
2017-05-10 21:07:00 +0300

    learning more ui: created a actionlistener

--- 2013-OOProgrammingWithJava-PART2/week11-week11_38.NoticeBoard/src/noticeboard/NoticeBoard.java
+++ 2013-OOProgrammingWithJava-PART2/week11-week11_38.NoticeBoard/src/noticeboard/NoticeBoard.java
@@ -11,8 +11,33 @@ public class NoticeBoard implements Runnable {
 
     @Override
     public void run() {
+
+        frame = new JFrame();
+        frame.setPreferredSize(new Dimension(400, 300));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+
     }
 
     private void createComponents(Container container) {
+        GridLayout layout = new GridLayout(3, 1);
+        container.setLayout(layout);
+
+        JTextField textTop = new JTextField("I was copied here from the JTextArea.");
+        JLabel labelBot = new JLabel();
+        JButton moveButton = new JButton("Copy!");
+
+        ActionEventListener mover = new ActionEventListener(textTop, labelBot);
+        moveButton.addActionListener(mover);
+
+        container.add(textTop);
+        container.add(moveButton);
+        container.add(labelBot);
+
     }
 }
